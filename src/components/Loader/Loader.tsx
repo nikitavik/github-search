8934477870f21c828995ba6d7ff1cab69c389e5c
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-04 01:17:36 +1000

    Add Loader Component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loader: React.FC = () => {
+  return <div className="d-flex justify-content-center m-4"><Spinner animation='border' variant='primary'/></div>
+}
+
+export default Loader
