6c8d757fe8484203eadfee959577048a9989e275
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-04 01:19:58 +1000

    Add routing with params on click on RepoItem card. +Fix some styles and rendering logic

--- src/components/RepoItem/RepoItem.tsx
+++ src/components/RepoItem/RepoItem.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ListGroup, Card } from 'react-bootstrap';
-
+import { useHistory } from 'react-router-dom';
 
 type RepositoryNode = {
-  description: string;
+  description?: string;
   id: string;
   nameWithOwner: string;
   stargazerCount: number;
-  primaryLanguage: { name: string; color: string };
+  primaryLanguage?: { name?: string; color?: string };
 };
 
 interface IRepoItemProps {
@@ -15,28 +15,36 @@ interface IRepoItemProps {
 }
 
 const RepoItem: React.FC<IRepoItemProps> = ({ repoInfo }) => {
+  const history = useHistory();
+  const openRepoHandler = () => {
+    history.push(`/repository/${repoInfo.id}`);
+  };
   return (
-    <ListGroup.Item>
-      <Card bsPrefix="list-group-item list-group-item-action">
+    <ListGroup.Item bsPrefix="border-none p-1">
+      <Card
+        bsPrefix="list-group-item list-group-item-action"
+        style={{ cursor: 'pointer' }}
+        onClick={openRepoHandler}
+      >
         <Card.Body>
           <Card.Title>{repoInfo.nameWithOwner}</Card.Title>
-          <Card.Text>{repoInfo.description}</Card.Text>
+          <Card.Text>{repoInfo.description || 'No description'}</Card.Text>
         </Card.Body>
         <Card.Footer bsPrefix="d-flex justify-content-between px-3">
-          <Card.Text bsPrefix="d-flex">
+          <Card.Text as="div" bsPrefix="d-flex">
             <div
               style={{
                 display: 'inline-block',
                 width: '20px',
                 height: '20px',
                 marginRight: '0.5rem',
-                background: repoInfo.primaryLanguage.color,
+                background: repoInfo.primaryLanguage?.color || '#111',
                 borderRadius: '50%',
               }}
             />
-            <span>{repoInfo.primaryLanguage.name}</span>
+            <span>{repoInfo.primaryLanguage?.name || 'Unknown'}</span>
           </Card.Text>
-          <Card.Text>
+          <Card.Text as="div">
             <span className="me-1">{repoInfo.stargazerCount}</span>
             <i className="far fa-star fa-lg" />
           </Card.Text>
