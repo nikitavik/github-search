35f483f3dc620b4489fe3a4754d6739a835f84fc
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-04 03:22:53 +1000

    Add query for fetching repo info by id

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -2,7 +2,12 @@ import { gql } from 'apollo-boost';
 
 const GET_SEARCH_RESULTS = gql`
   query ($searchQuery: String!, $pageSize: Int, $afterCursor: String) {
-    search(query: $searchQuery, type: REPOSITORY, first: $pageSize, after: $afterCursor) {
+    search(
+      query: $searchQuery
+      type: REPOSITORY
+      first: $pageSize
+      after: $afterCursor
+    ) {
       repositoryCount
       edges {
         node {
@@ -27,4 +32,33 @@ const GET_SEARCH_RESULTS = gql`
   }
 `;
 
-export { GET_SEARCH_RESULTS };
+const GET_REPO_BY_ID = gql`
+  query ($repoId: ID!) {
+    node(id: $repoId) {
+      ... on Repository {
+        id
+        nameWithOwner
+        description
+        pullRequests {
+          totalCount
+        }
+        issues {
+          totalCount
+        }
+        languages(first: 100) {
+          nodes {
+            color
+            name
+          }
+        }
+        primaryLanguage {
+          name
+          color
+        }
+        stargazerCount
+      }
+    }
+  }
+`;
+
+export { GET_SEARCH_RESULTS, GET_REPO_BY_ID };
