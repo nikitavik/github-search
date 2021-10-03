9af2296c2083131d494a8a9f47fb3d60d881ee38
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-04 01:14:25 +1000

    Created button component

---
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+interface IPrimaryButtonProps {
+  onClick: () => void
+  text: string
+  size: "lg" | "sm" | undefined
+  color: string
+  disabled?: boolean
+}
+
+const PrimaryButton: React.FC<IPrimaryButtonProps> = ({onClick, text, color, size, disabled}) => {
+  return(
+    <Button color={color} size={size} onClick={onClick} disabled={disabled}>
+      {text}
+    </Button>
+  )
+}
+
+export default PrimaryButton
